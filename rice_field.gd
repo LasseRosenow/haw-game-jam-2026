c208079ea89a52eefe626de317dc1be0799cc5d7
AnnsAnn <git@annsann.eu>
2026-06-20 19:12:28 +0200

    Rice field fully working

--- rice_field.gd
+++ rice_field.gd
@@ -3,7 +3,7 @@ extends RigidBody2D
 @export var type = "RiceField"
 
 signal interacted_with
-@export_enum("dry", "stage1", "stage2", "watered") var stage: String = "dry"
+@export_enum("dry", "stage1", "stage2", "harvested", "watered") var stage: String = "dry"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -19,9 +19,14 @@ func _on_interacted_with() -> void:
 	if stage == "dry":
 		stage = "watered"
 		$ButtonUI.emit_signal("change_animation", "Wait")
+		$ButtonUI.emit_signal("start_animation", true)
 		$GrowTimer.start(5)
-	
-	$AnimatedSprite2D.play(stage)
+		$AnimatedSprite2D.play(stage)
+	elif stage == "stage2":
+		stage = "harvested"
+		$AnimatedSprite2D.play("watered")
+		$ButtonUI.emit_signal("start_animation", false)
+		$GrowTimer.start(10)
 
 
 func _on_grow_timer_timeout() -> void:
@@ -33,5 +38,9 @@ func _on_grow_timer_timeout() -> void:
 		$GrowTimer.start(5)
 	elif stage == "stage2":
 		$ButtonUI.emit_signal("change_animation", "Done")
-
+	elif stage == "harvested":
+		stage = "dry"
+		$ButtonUI.emit_signal("change_animation", "Water")
+		$ButtonUI.emit_signal("start_animation", true)
+		
 	$AnimatedSprite2D.play(stage)
