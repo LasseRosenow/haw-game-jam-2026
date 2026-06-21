c84a4c5686bd11fe68f3a214b10a7bf7eaeaee00
AnnsAnn <git@annsann.eu>
2026-06-21 15:07:02 +0200

    Credit

--- main_menu.gd
+++ main_menu.gd
@@ -17,3 +17,7 @@ func _on_start_pressed() -> void:
 
 func _on_audio_stream_player_finished() -> void:
 	$AudioStreamPlayer.play(0.0)
+
+
+func _on_credits_pressed() -> void:
+	get_tree().change_scene_to_file("res://Credits.tscn")
